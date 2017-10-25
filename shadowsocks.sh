1e1fbddaa35f506b3c9df4853ece0ab933add972
shenyunhang <yhshen@stu.xmu.edu.cn>
2017-10-25 09:48:53 +0800

    OptiPlex-3020@2017.10.25: add shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -3,4 +3,8 @@
 set -x
 set -e
 
-sh <(curl https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh)
+wget –no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks.sh -O /tmp/shadowsocks.sh
+chmod +x /tmp/shadowsocks.sh
+sudo /tmp/shadowsocks.sh
+
+echo "You should ppen socks of TCP for shadowsocks"
