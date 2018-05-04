ef8fc271305aeba54c5b7da94983639c4d072d11
shenyunhang <yhshen@stu.xmu.edu.cn>
2018-05-04 15:02:01 +0800

    OptiPlex-3020@2018.05.04:

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,10 @@ sudo apt-get install -y python-pip
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 sudo dpkg-reconfigure locales
-sudo pip install apt-select
-apt-select -C CN -c -t 3 -m one-week-behind
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup && sudo mv sources.list /etc/apt/
+
+#sudo pip install apt-select
+#apt-select -C CN -c -t 3 -m one-week-behind
+#sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup && sudo mv sources.list /etc/apt/
 
 # 添加仓库
 ./add_repository.sh
@@ -30,5 +31,5 @@ sudo update-rc.d ssh defaults
 ./powerline.sh
 ./vim.sh
 ./tmux.sh
-./caffe.sh
+#./caffe.sh
 ./zsh.sh
