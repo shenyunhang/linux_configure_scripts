fbeebfd1a93b585240899999c8a00095cfb391df
shenyunhang <yhshen@stu.xmu.edu.cn>
2018-12-26 10:11:43 +0000

    ubuntu 18.04 support

--- opencv.sh
+++ opencv.sh
@@ -4,7 +4,8 @@ set -x
 
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+#sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 sudo apt-get install -y liblapacke-dev checkinstall
 
 cd ~/Documents
