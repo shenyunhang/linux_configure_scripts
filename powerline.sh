e3be2b37ef565caac0be7b5a76760e046e91f956
shenyunhang <shenyunhang01@gmail.com>
2019-07-01 14:41:48 +0800

    shenyunhang-Precision-Tower-7910@2019-07-01_14-41-48:

--- powerline.sh
+++ powerline.sh
@@ -5,15 +5,17 @@ set -e
 
 sudo -H pip install powerline-status
 
-wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
-wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+#wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+#wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+cd /tmp
+git clone https://github.com/powerline/powerline.git
 
 mkdir -p ~/.fonts/
-mv PowerlineSymbols.otf ~/.fonts/
+cp /tmp/powerline/font/PowerlineSymbols.otf ~/.fonts/
 #sudo mv PowerlineSymbols.otf /usr/share/fonts/X11/misc
 
 fc-cache -vf ~/.fonts/
 #sudo fc-cache -vf /usr/share/fonts/X11/misc
 
 mkdir -p ~/.config/fontconfig/conf.d/
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+cp /tmp/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
