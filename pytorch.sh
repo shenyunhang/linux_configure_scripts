2a40867c439c577e9cc7e40c3ac2dd5b0e830537
shenyunhang <yhshen@stu.xmu.edu.cn>
2019-09-15 12:40:57 -0400

    macww-PowerEdge-T630@2019-09-15_12-40-57:

--- pytorch.sh
+++ pytorch.sh
@@ -6,36 +6,36 @@ set -x
 cd ~/Documents
 mkdir -p pytorch
 
+echo "Installing pytorch"
+cd ~/Documents/pytorch
 if [ ! -d ~/Documents/pytorch/pytorch ]; then
-	echo "we need install pytorch"
-
-	cd ~/Documents/pytorch
 	git clone https://github.com/pytorch/pytorch.git
-	cd pytorch
-	git checkout v1.2.0
-
-	sudo pip3 install -r requirements.txt
-
-	git submodule sync
-	git submodule update --init --recursive
-	sudo USE_OPENCV=On USE_LMDB=On BUILD_BINARY=On python3 setup.py install
 fi
+cd pytorch
+git checkout v1.2.0
 
+sudo pip3 install -r requirements.txt
+
+git submodule sync
+git submodule update --init --recursive
+sudo USE_OPENCV=On USE_LMDB=On BUILD_BINARY=On python3 setup.py install
+
+
+echo "Installing vision"
+cd ~/Documents/pytorch
 if [ ! -d ~/Documents/pytorch/vision ]; then
-	echo "we need install vision"
-
-	cd ~/Documents/pytorch
 	git clone https://github.com/pytorch/vision.git
-	cd vision
-	git checkout v0.4.0
-
-	git submodule sync
-	git submodule update --init --recursive
-	sudo python3 setup.py install
 fi
+cd vision
+git checkout v0.4.0
 
+git submodule sync
+git submodule update --init --recursive
+sudo python3 setup.py install
+
+
+echo "Installing examples"
+cd ~/Documents/pytorch
 if [ ! -d ~/Documents/pytorch/examples ]; then
-	echo "we need install examples"
-	cd ~/Documents/pytorch
 	git clone https://github.com/pytorch/examples.git
 fi
