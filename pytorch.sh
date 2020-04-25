7378f50b6b2b5236805c557ea142f5da77eeec44
shenyunhang <shenyunhang01@gmail.com>
2020-04-25 15:59:42 +0800

    mac-u1s2@2020-04-25_15-59-42:

--- pytorch.sh
+++ pytorch.sh
@@ -9,6 +9,9 @@ set -x
 #####################################################################
 #####################################################################
 
+alias python='/usr/bin/python3.6'
+alias pip='/usr/local/bin/pip3.6'
+
 cd ~/Documents
 mkdir -p pytorch
 
@@ -16,31 +19,42 @@ echo "Installing pytorch"
 cd ~/Documents/pytorch
 if [ ! -d ~/Documents/pytorch/pytorch ]; then
 	git clone https://github.com/pytorch/pytorch.git
-fi
-cd pytorch
-git checkout v1.3.1
+	cd pytorch
+else
+	cd pytorch
+	git fetch origin
 
-sudo pip3 install -r requirements.txt
-sudo pip3 install -r caffe2/requirements.txt
-sudo pip3 install protobuf
+fi
+sudo rm -rf build
+rm -r ./*
+sudo git reset --hard HEAD
+git checkout v1.5.0
+
+sudo pip install -r requirements.txt
+sudo pip install -r caffe2/requirements.txt
+sudo pip install protobuf
 
 git submodule sync
 git submodule update --init --recursive
-sudo rm -rf build
-sudo MAX_JOBS=4 USE_OPENCV=On USE_LMDB=On BUILD_BINARY=On python3 setup.py install
+sudo MAX_JOBS=4 USE_OPENCV=On USE_LMDB=On BUILD_BINARY=On python setup.py install
 
 
 echo "Installing vision"
 cd ~/Documents/pytorch
 if [ ! -d ~/Documents/pytorch/vision ]; then
 	git clone https://github.com/pytorch/vision.git
+	cd vision
+else
+	cd vision
+	git fetch origin
 fi
-cd vision
-git checkout v0.4.1
+#git checkout v0.5.0
+git checkout origin/master
 
 git submodule sync
 git submodule update --init --recursive
-sudo python3 setup.py install
+sudo rm -rf build
+sudo MAX_JOBS=4 python setup.py install
 
 
 echo "Installing examples"
