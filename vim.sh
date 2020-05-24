a588faef0f08edb5aa29463939382eb344f2e805
shenyunhang <yhshen@stu.xmu.edu.cn>
2020-05-24 09:16:50 -0400

    mac-u2s2@2020-05-24_09-16-50:

--- vim.sh
+++ vim.sh
@@ -23,4 +23,4 @@ make -j8
 sudo make install
 
 cd ~
-#sh <(curl -sSL https://raw.github.com/shenyunhang/vimrc/master/bootstrap.sh -L)
+sh <(curl -sSL https://raw.github.com/shenyunhang/vimrc/master/bootstrap.sh -L)
